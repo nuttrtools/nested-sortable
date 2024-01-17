016ff5bc9ad1512e84e85fef3230dcb44194e504
Akhil Ravindran <imakhilravindran@gmail.com>
2024-01-17 23:55:35 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { NestedSortable } from "./Tree/NestedSortable"
+
+function App() {
+
+  return (
+    <>
+      <NestedSortable collapsible/>
+    </>
+  )
+}
+
+export default App
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { NestedSortable } from './Tree/NestedSortable';
